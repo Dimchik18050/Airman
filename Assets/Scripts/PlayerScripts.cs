c268d3fd2e2f5ed118b013cf50dc1c0922dcb407
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2022-05-30 17:35:58 +0300

    Controller for moving player Created and added script for obj player.

---
+++ Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Контролер та поведінка гравця
+
+public class PlayerScripts : MonoBehaviour
+{
+    // Швидкість руху 
+    public Vector2 speed = new Vector2(50, 50);
+
+    // Оголошення rigid2D
+    Rigidbody2D rigid2D;
+
+    void Start ()
+    {
+        // Ініціалізування rigid2D
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Напрямок руху
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Отримання інформації осі
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // движение в каждом направлении
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+
+    }
+
+    // Викликається кожного разу через певну кількість кадрів
+    void FixedUpdate()
+    {
+        // переміщення ігрового об'єкту
+        rigid2D.velocity = movement;
+
+    }
+}
